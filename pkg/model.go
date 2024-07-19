049967f179b84fa0ac281a8f4690397857f4fc0f
lhdht <15229684090@163.com>
2024-07-19 09:33:11 +0800

    update:config

--- pkg/model.go
+++ pkg/model.go
@@ -13,8 +13,6 @@ type CoreEntity struct {
 type ConfigEntity struct {
 	Namespace string `json:"namespace" yaml:"namespace" mapstructure:"namespace"`
 	Endpoint  string `json:"endpoint" yaml:"endpoint" mapstructure:"endpoint"`
-	MaxRetry  uint   `json:"max_retry" yaml:"max_retry" mapstructure:"max_retry"`
-	TTL       int64  `json:"ttl" yaml:"ttl" mapstructure:"ttl"`
 	DNS       string `json:"dns" yaml:"dns" mapstructure:"dns"`
 	Run       string `json:"run" yaml:"run" mapstructure:"run"`
 }
