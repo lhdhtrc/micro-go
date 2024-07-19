eaf4200c55adfd524475f3d34acf13e94ebb36f9
lhdht <15229684090@163.com>
2024-07-19 09:15:09 +0800

    update:config

--- pkg/model.go
+++ pkg/model.go
@@ -9,3 +9,12 @@ type CoreEntity struct {
 	server *grpc.Server
 	logger *zap.Logger
 }
+
+type ConfigEntity struct {
+	Namespace string `json:"namespace"`
+	Endpoint  string `json:"endpoint"`
+	MaxRetry  uint   `json:"max_retry"`
+	TTL       int64  `json:"ttl"`
+	DNS       string `json:"dns"`
+	Run       string `json:"run"`
+}
