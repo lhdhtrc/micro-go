2bd46fd96ce694e6a0ad087a74a20246ee09f02a
lhdht <15229684090@163.com>
2024-12-01 18:39:59 +0800

    feature: 测试用例

--- support/etcd/discover_test.go
+++ support/etcd/discover_test.go
@@ -1,1 +1,51 @@
 package etcd
+
+import (
+	"fmt"
+	micro "github.com/lhdhtrc/micro-go/core"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"testing"
+)
+
+func TestDiscover(t *testing.T) {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints: []string{"192.168.1.100:10206"},
+		Username:  "root",
+		Password:  "123456",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cli.Close()
+
+	// 创建一个服务配置
+	config := &micro.ServiceConfig{
+		AppId:         "test-app",
+		OuterNetIp:    "127.0.0.1",
+		InternalNetIp: "127.0.0.1",
+		Namespace:     "test-namespace",
+		TTL:           10,
+		MaxRetry:      3,
+	}
+
+	// 初始服务发现实例
+	dis, err := NewDiscover(cli, config)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// 使用日志, 非必要不要启用
+	dis.WithLog(func(level micro.LogLevel, message string) {
+		fmt.Println("log", level, message)
+	})
+
+	go dis.Watcher()
+
+	fmt.Println("测试完毕")
+
+	// 非必须
+	for {
+
+	}
+}
