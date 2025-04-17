21c5a54bd2bad3f2c25c4f867b34db7edcce939f
lhdht <15229684090@163.com>
2025-04-17 21:51:38 +0800

    fix: 修改

--- pkg/core/gateway.go
+++ pkg/core/gateway.go
@@ -2,9 +2,5 @@ package micro
 
 type GatewayConf struct {
 	// 网卡
-	Network string `json:"network"`
-	// 外网地址
-	OuterNetAddr string `json:"outer_net_addr"`
-	// 内网地址
-	InternalNetAddr string `json:"internal_net_addr"`
+	Network Network `json:"network"`
 }
