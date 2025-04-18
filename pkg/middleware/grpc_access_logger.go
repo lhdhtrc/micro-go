eb264751adc9876b7acb87316c89985c5740b2a8
lhdht <15229684090@163.com>
2025-04-18 11:20:47 +0800

    fix: 修改

--- pkg/middleware/grpc_access_logger.go
+++ pkg/middleware/grpc_access_logger.go
@@ -28,37 +28,7 @@ func GrpcAccessLogger(handle func(b []byte, msg string)) grpc.UnaryServerInterce
 		if handle != nil {
 			loggerMap := make(map[string]interface{})
 
-			traceId := md.Get("trace-id")
-			if len(traceId) != 0 {
-				loggerMap["trace_id"] = traceId[0]
-			} else {
-				loggerMap["trace_id"] = uuid.New().String()
-			}
-
-			ip := md.Get("x-real-ip")
-			if len(ip) != 0 {
-				loggerMap["ip"] = ip[0]
-			}
-
-			clientName := md.Get("client-name")
-			if len(clientName) != 0 {
-				loggerMap["client_name"] = clientName[0]
-			}
-
-			clientType := md.Get("client-type")
-			if len(clientName) != 0 {
-				loggerMap["client_type"] = clientType[0]
-			}
-
-			clientSystem := md.Get("client-system")
-			if len(clientSystem) != 0 {
-				loggerMap["client_system"] = clientSystem[0]
-			}
-
-			loggerMap["status"] = 200
-			loggerMap["timer"] = elapsed.String()
-
-			loggerMap["method"] = "GRPC"
+			loggerMap["method"] = "REQUEST_METHOD_GRPC"
 			loggerMap["path"] = info.FullMethod
 
 			request, _ := json.Marshal(req)
@@ -66,6 +36,66 @@ func GrpcAccessLogger(handle func(b []byte, msg string)) grpc.UnaryServerInterce
 
 			response, _ := json.Marshal(resp)
 			loggerMap["response"] = string(response)
+
+			loggerMap["duration"] = elapsed.Milliseconds()
+			loggerMap["status"] = 200
+
+			ip := md.Get("x-real-ip")
+			if len(ip) != 0 {
+				loggerMap["ip"] = ip[0]
+			}
+
+			systemType := md.Get("system-type")
+			if len(systemType) != 0 {
+				loggerMap["system_type"] = systemType[0]
+			}
+
+			systemName := md.Get("system-name")
+			if len(systemName) != 0 {
+				loggerMap["system_name"] = systemName[0]
+			}
+
+			systemVersion := md.Get("system-version")
+			if len(systemVersion) != 0 {
+				loggerMap["system_version"] = systemVersion[0]
+			}
+
+			clientType := md.Get("client-type")
+			if len(clientType) != 0 {
+				loggerMap["client_type"] = clientType[0]
+			}
+
+			clientName := md.Get("client-name")
+			if len(clientName) != 0 {
+				loggerMap["client_name"] = clientName[0]
+			}
+
+			clientVersion := md.Get("client-version")
+			if len(clientVersion) != 0 {
+				loggerMap["client_version"] = clientVersion[0]
+			}
+
+			appId := md.Get("app-id")
+			if len(appId) != 0 {
+				loggerMap["app_id"] = appId[0]
+			}
+
+			appVersion := md.Get("app-version")
+			if len(appVersion) != 0 {
+				loggerMap["app_version"] = appVersion[0]
+			}
+
+			deviceFormFactor := md.Get("device-form-factor")
+			if len(deviceFormFactor) != 0 {
+				loggerMap["device_form_factor"] = deviceFormFactor[0]
+			}
+
+			traceId := md.Get("trace-id")
+			if len(traceId) != 0 {
+				loggerMap["trace_id"] = traceId[0]
+			} else {
+				loggerMap["trace_id"] = uuid.New().String()
+			}
 
 			b, _ := json.Marshal(loggerMap)
 			handle(b, fmt.Sprintf("[%s] [GRPC]:[%s] [%s]-[%d]\n", time.Now().Format(time.DateTime), info.FullMethod, elapsed.String(), status))
