270f1a5f97725725e39cc9f4fde0f2e84ab88aa2
lhdht <15229684090@163.com>
2025-04-18 15:44:54 +0800

    fix: 修改

--- pkg/core/network.go
+++ pkg/core/network.go
@@ -1,7 +1,24 @@
 package micro
 
+import (
+	"net"
+	"strings"
+)
+
 type Network struct {
 	SN       string `json:"sn"`
 	Internal string `json:"internal"`
 	External string `json:"external"`
+}
+
+// GetInternalNetworkIp 获取内网ip
+func GetInternalNetworkIp() string {
+	dial, err := net.Dial("udp", "114.114.114.114:80")
+	if err != nil {
+		return "127.0.0.1"
+	}
+	addr := dial.LocalAddr().String()
+
+	index := strings.LastIndex(addr, ":")
+	return addr[:index]
 }
