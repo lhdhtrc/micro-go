b9b2dc6e668cb016830b74cceda7201ce955d00d
lhdht <15229684090@163.com>
2025-06-16 16:21:20 +0800

    perfect：优化远程调用方法

--- pkg/core/remote_invoke.go
+++ pkg/core/remote_invoke.go
@@ -4,23 +4,17 @@ import (
 	"errors"
 )
 
-// InvokeResponse 远程调用响应
-type InvokeResponse struct {
-	Code    uint32 `json:"code"`
-	Message string `json:"message"`
-}
-
 // WithRemoteInvoke 远程调用
-func WithRemoteInvoke[T any](callFunc func() (T, *InvokeResponse, error)) (T, error) {
+func WithRemoteInvoke[T any](callFunc func() (data T, code uint32, message string, err error)) (T, error) {
 	// 执行服务调用
-	data, res, err := callFunc()
+	data, code, message, err := callFunc()
 	if err != nil {
 		return data, err
 	}
 
 	// 检查响应状态码
-	if res.Code != 200 {
-		return data, errors.New(res.Message)
+	if code != 200 {
+		return data, errors.New(message)
 	}
 
 	return data, nil
