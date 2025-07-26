2506cd8fc68138279bcd65634f3cc3d9d7d6b62a
lhdht <15229684090@163.com>
2025-07-26 08:47:55 +0800

    fix: 修改日志中间件

--- pkg/middleware/access_logger.go
+++ pkg/middleware/access_logger.go
@@ -42,26 +42,25 @@ func GrpcAccessLogger(handle func(b []byte, msg string)) grpc.UnaryServerInterce
 			loggerMap["duration"] = elapsed.String()
 			loggerMap["status"] = 200
 
-			loggerMap["ip"], err = micro.ParseMetaKey(md, "client-ip")
+			loggerMap["ip"], _ = micro.ParseMetaKey(md, "client-ip")
 
 			loggerMap["system_name"], _ = micro.ParseMetaKey(md, "system-name")
 			loggerMap["client_name"], _ = micro.ParseMetaKey(md, "client-name")
 
-			var systemType, clientType, deviceFormFactor string
-			systemType, err = micro.ParseMetaKey(md, "system-type")
-			if err == nil {
+			systemType, se := micro.ParseMetaKey(md, "system-type")
+			if se == nil {
 				loggerMap["system_type"] = 0
 			} else {
 				loggerMap["system_type"], _ = strconv.ParseInt(systemType, 10, 32)
 			}
-			clientType, err = micro.ParseMetaKey(md, "client-type")
-			if err == nil {
+			clientType, ce := micro.ParseMetaKey(md, "client-type")
+			if ce == nil {
 				loggerMap["client_type"] = 0
 			} else {
 				loggerMap["client_type"], _ = strconv.ParseInt(clientType, 10, 32)
 			}
-			deviceFormFactor, err = micro.ParseMetaKey(md, "device-form-factor")
-			if err == nil {
+			deviceFormFactor, de := micro.ParseMetaKey(md, "device-form-factor")
+			if de == nil {
 				loggerMap["device_form_factor"] = deviceFormFactor
 			} else {
 				loggerMap["device_form_factor"] = 0
@@ -71,10 +70,11 @@ func GrpcAccessLogger(handle func(b []byte, msg string)) grpc.UnaryServerInterce
 			loggerMap["client_version"], _ = micro.ParseMetaKey(md, "client-version")
 			loggerMap["app_version"], _ = micro.ParseMetaKey(md, "app-version")
 
-			loggerMap["trace_id"], err = micro.ParseMetaKey(md, "trace-id")
-			if err != nil {
-				loggerMap["trace_id"] = uuid.New().String()
+			traceId, le := micro.ParseMetaKey(md, "trace-id")
+			if le != nil {
+				traceId = uuid.New().String()
 			}
+			loggerMap["trace_id"] = traceId
 			loggerMap["app_id"], _ = micro.ParseMetaKey(md, "app-id")
 
 			b, _ := json.Marshal(loggerMap)
