fc55caad63b9f6c6bb75aae705200a00a57a2add
lhdht <15229684090@163.com>
2025-11-15 10:56:10 +0800

    fix: amend

--- pkg/middleware/context.go
+++ pkg/middleware/context.go
@@ -6,7 +6,7 @@ import (
 	"google.golang.org/grpc/metadata"
 )
 
-// GrpcContextConverter 将入站上下文转换为出战上下文
+// GrpcContextConverter 将入站上下文转换为出站上下文
 func GrpcContextConverter(ctx context.Context, req interface{}, _ *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 	md, _ := metadata.FromIncomingContext(ctx)
 	oc := metadata.NewOutgoingContext(ctx, md.Copy())
